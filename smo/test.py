fea8b3b022da8d5beb302c4e85579df666424f5d
light1726 <lu-h17@mails.tsinghua.edu.cn>
2017-12-18 00:15:33 +0800

    modified test

--- smo/test.py
+++ smo/test.py
@@ -1,9 +1,19 @@
 import svmMLiA
 import time
+import pickle
+
 if __name__ == "__main__":
     start = time.time()
-    kernel =['lin', 'rbf']
+    kernel = ['lin', 'rbf']
     alphas_all, b_all = svmMLiA.train_step(kTup=('rbf', 10))
+
+    # saving parameter
+    with open('alpha_b.pkl', 'wb') as f:
+        pickle.dump([alphas_all, b_all], f)
+
+    # Getting back the objects:
+    with open('alpha_b.pkl', 'rb') as f:
+        alphas_all, b_all = pickle.load(f)
 
     svmMLiA.predict_digits(alphas_all, b_all, kTup=('rbf', 10))
     end = time.time()
